e9dec0b8e6b2f8fc27bf841baa9840c40b50856d
IsaacK47 <isaackayombo41@gmail.com>
2025-04-11 09:13:42 +0200

    Creating Reducer function using switch/case statements

--- index.js
+++ index.js
@@ -3,5 +3,20 @@
 const ADD = 'ADD';
 const SUBTRACT = 'SUBTRACT';
 const RESET = 'RESET';
-
 // -----------------
+
+// THE TALLY APP REDUCER
+
+function theReducer(state = { count: 0 }, action) {
+    switch (action.type) {
+        case ADD:
+            return { count: state.count + 1 };
+        case SUBTRACT:
+            return { count: state.count - 1};
+        case RESET:
+            return { count: 0};
+        default:
+            return state;
+    }
+}
+// ------------------
